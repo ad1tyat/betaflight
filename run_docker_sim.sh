29c8563e2d0c021f5805690a6236922502035b49
ad1tyat <adityatrivedi25@gmail.com>
2021-08-08 16:26:14 +0530

    changed docker run script

--- run_docker_sim.sh
+++ run_docker_sim.sh
@@ -2,4 +2,4 @@
 
 # docker exec -it sitl bash # to open a second terminal
 echo "Run: 'gazebo --verbose ./iris_arducopter_demo.world' inside the container".
-docker run --rm --name sitl -it --gpus all --user $(id -u):$(id -g) --group-add sudo -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/home/sitl_user/host --device /dev/dri/  --network host --workdir="/home/sitl_user/ardupilot_gazebo/" ricardodeazambuja/gazebo9_cudagl_sitl:latest bash #./start_sim.sh
+sudo docker run --rm --name sitl -it --gpus all --user $(id -u):$(id -g) --group-add sudo -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd):/home/sitl_user/host --device /dev/dri/  --network host --workdir="/home/sitl_user/ardupilot_gazebo/" ricardodeazambuja/gazebo9_cudagl_sitl:latest bash #./start_sim.sh
